110eb49d7f3d8a0baf4d6f52952c818ab8f11162
razavismfd <razavismfd@gmail.com>
2024-05-09 23:48:31 +0330

    updated comments for fileuploadview, added authentication to fileupload API, adjusted file directory, listed all files related to a part, created download link for single file, prepare some message for the uploads without parts parameter

--- todo_api/urls.py
+++ todo_api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
+from django.urls import re_path as url
+from django.conf import settings
 
 from .views import (
     AutomobileListApiView,
     PartsListApiView,
     FileUploadView,
+    PartFilesListApiView,
+    download,
 )
 
 
@@ -11,5 +15,7 @@ from .views import (
 urlpatterns = [
     path('api', AutomobileListApiView.as_view()),
     path('parts', PartsListApiView.as_view()),
-    path('upload', FileUploadView.as_view())
+    path('upload', FileUploadView.as_view()),
+    path('list_of_part_files', PartFilesListApiView.as_view()),
+    url(r'^download/(?P<path>.*)$', download),
 ]
--- todo_api/views.py
+++ todo_api/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
 from rest_framework.parsers import MultiPartParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -81,24 +83,25 @@ class PartsListApiView(APIView):
 
 
 class FileUploadView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
     parser_classes = (MultiPartParser, )
 
     def post(self, request, format=None):
         '''
         to check this one you can enjoy some curl like below:
-        curl -X POST -H "Content-Type:multipart/form-data" -u admin:user -F "file=/Users/sayedmohmmadrazavi/Desktop/photo.jpg" http://127.0.0.1:8000/autos/upload?manufacturer=manufacturer1
+        curl -X POST -H "Content-Type:multipart/form-data" -u admin:user -F "file=@/Users/sayedmohmmadrazavi/Desktop/photo.jpg" http://127.0.0.1:8000/autos/upload?parts=1
         '''
         parts = request.query_params.get('parts')
         if parts:
             file_obj = request.FILES['file']
 
-            tmp_address = os.path.join(f'media/' + parts + '/', file_obj.name)
+            tmp_address = os.path.join(f'media/' + parts + '/')
             fss = FileSystemStorage(location=tmp_address, base_url=tmp_address)
             filename = fss.save(file_obj.name, file_obj)
             url = fss.url(filename)
             data = {
                 'file': file_obj,
-                'parts': parts,
+                'parts': int(parts),
             }
             serializer = PartFileSerializer(data=data)
             if serializer.is_valid():
@@ -106,4 +109,33 @@ class FileUploadView(APIView):
                 return Response(file_obj.name, status=status.HTTP_201_CREATED)
 
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response('No parts found in query parameters.', status=status.HTTP_400_BAD_REQUEST)
+
+
+class PartFilesListApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    # 1. List all
+    def get(self, request, *args, **kwargs):
+        '''
+        List all the  items for given requested user
+        '''
+        # self.request = request
+        # automobile = self.get_queryset()
+        part_id = self.request.query_params.get('part_id')
+        if part_id:
+            parts = PartFile.objects.filter(parts__parts_id__contains=part_id)
+            serializer = PartFileSerializer(parts, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+def download(request, path):
+    file_path = os.path.join(settings.MEDIA_ROOT, path)
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+            return response
+    raise Response(status=status.HTTP_404_NOT_FOUND)
